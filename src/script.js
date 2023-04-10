88f42d17821634499eed752ea87a87cb5d023877
joisv <joisvvv@hotmail.com>
2023-04-10 19:20:05 +0700

    memperbaiki tab pada main

--- src/script.js
+++ src/script.js
@@ -40,3 +40,21 @@ const handleButtonGenre = () => {
     genre.classList.toggle('grid')
     genreSvg.classList.toggle('rotate-[]')
 }
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "grid";
+  evt.currentTarget.className += " active";
+}
+
+// Set the default tab to be active
+document.getElementById("defaultOpen").click();
+
